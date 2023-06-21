0cc99f46c86bd9c43a8b170ca3da1f59c4f1d2ea
revolutionartist <joydanah17@gmail.com>
2023-06-21 20:02:58 +0300

    Implemented movement functionality for Dodger character

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+// Get a reference to the Dodger element
+var dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  // Get the current left position of the Dodger
+  var currentLeft = parseInt(dodger.style.left);
+
+  // Calculate the new left position by subtracting a certain value (e.g., 10 pixels)
+  var newLeft = currentLeft - 10;
+
+  // Set the new left position for the Dodger
+  dodger.style.left = newLeft + "px";
+}
+
+function moveDodgerRight() {
+  // Get the current left position of the Dodger
+  var currentLeft = parseInt(dodger.style.left);
+
+  // Calculate the new left position by adding a certain value (e.g., 10 pixels)
+  var newLeft = currentLeft + 10;
+
+  // Set the new left position for the Dodger
+  dodger.style.left = newLeft + "px";
+}
+
